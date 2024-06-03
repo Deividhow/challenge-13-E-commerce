84d6196f781bbea041bab800957b10cae910b1d7
deivid how <vvdoxi@gmail.com>
2024-06-02 22:31:14 -0400

    fixed the delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Product, product, Tag, ProductTag, Category } = require('../../models');
+const { Product, Tag, ProductTag, Category } = require('../../models');
 
 // The `/api/products` endpoint
 
@@ -116,8 +116,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Product deleted successfully' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
